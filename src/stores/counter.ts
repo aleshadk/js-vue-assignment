d877ae6978f7bf3c54b836a129a1d5ef96154a93
aleksey diduh <aleksey.diduh@skyeng.ru>
2025-01-22 19:19:32 -0300

    clean up project

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,11 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useMainStore = defineStore('main', () => {
+  // state
 
-  return { count, doubleCount, increment }
+  // getters
+
+  // actions
+
+  return {}
 })
